6fbfee481e6407a2f473504c2c1c6911d9ff17ab
Eurico Serradas <euricosd@gmail.com>
2026-09-19 16:19:13 +0100

    Corrected hasContactsSameNumber() logic

--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -76,11 +76,11 @@ public class ContactBook {
     }
 
 	public boolean hasContactsSameNumber() {
-		if (contacts.length <= 1)
+		if (counter <= 1)
 			return false;
 
-		for (int i = 0; i < contacts.length; i++)
-			for (int j = i + 1; j < contacts.length; j++)
+		for (int i = 0; i < counter; i++)
+			for (int j = i + 1; j < counter; j++)
 				if (contacts[i].getPhone() == contacts[j].getPhone())
 					return true;
 
